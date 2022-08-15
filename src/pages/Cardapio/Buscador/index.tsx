817a06cb8a1f9ad32a337f3a0c773a50697b95be
CarlosLincon <carloslincon29@gmail.com>
2022-08-14 22:24:52 -0300

    mensagem

---
+++ src/pages/Cardapio/Buscador/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+interface Props {
+    busca: string;
+    setBusca: React.Dispatch<React.SetStateAction<string>>;
+
+}
+
+export default function Buscador({busca,setBusca}: Props) {
+    return (
+        <div></div>
+    )
+}
